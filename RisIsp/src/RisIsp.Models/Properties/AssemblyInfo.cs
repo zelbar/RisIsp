92dfeb751c00a8a48bc7aee702ae32a102979c23
zeljko-b <zeljko.baranek@fer.hr>
2017-05-15 21:59:28 +0200

    Initial projects in solution.
    Solution with following projects: CoreLib, Dal, Bll, Models, WebMvc

---
+++ RisIsp/src/RisIsp.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RisIsp.Models")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("504bd01b-5ee5-4cea-9ce4-2a7b0785f204")]
